a381b9ca76794b9a09bf16fbc0bab74f94e732c6
taichi221228 <taichi221228@icloud.com>
2022-07-15 20:12:06 +0900

    :broom: Format the code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {},
   },
