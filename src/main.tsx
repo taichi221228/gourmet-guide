9162843a82fad45958df58fb138dcc450e4a0968
taichi221228 <taichi221228@icloud.com>
2022-07-15 18:38:06 +0900

    :package: Add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'sanitize.css/assets.css';
-import 'sanitize.css/forms.css';
-import 'sanitize.css/reduce-motion.css';
-import 'sanitize.css/system-ui.css';
-import 'sanitize.css/typography.css';
-import 'sanitize.css/ui-monospace.css';
 import './index.css';
 import App from './App';
 
